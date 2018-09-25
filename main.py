25d6d7c8a5be9e43ca430a30cc3850fc8c998c54
Миша <mvbdoc1@gmail.com>
2018-09-25 23:14:46 +0300

    250918

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from PIL import Image, ImageDraw
+# https://habr.com/post/163663/
 mode = int(input('mode:')) #Считываем номер преобразования.
 image = Image.open("temp.jpg") #Открываем изображение.
