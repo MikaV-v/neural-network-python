c0779f1548f8a15f52cf730b0590092865076723
Миша <mvbdoc1@gmail.com>
2018-09-25 22:58:43 +0300

    250918

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import math
 import image
 import random
 import PIL
+import os
+
+import Image, ImageDraw
+import os
+image = Image.open("red_410.png")
+width = image.size[0]
+height = image.size[1]
+pix = image.load()
+a = pix[4,4][0]
+b = pix[4,4][1]
+c = pix[4,4][2]
+if (a+b+c<400):
+    print('R=',a,'G=',b,'B=',c)
+    print (os.path.realpath("red_410.png") )
+else:
+    print("Noooo")
