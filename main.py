50f47e6b61b609e119ca0813c26e629854dff19f
Миша <mvbdoc1@gmail.com>
2018-09-25 23:09:04 +0300

    250918

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-import math
-import image
-import random
-import PIL
-import os
-
-import Image, ImageDraw
-import os
-image = Image.open("red_410.png")
-width = image.size[0]
-height = image.size[1]
-pix = image.load()
-a = pix[4,4][0]
-b = pix[4,4][1]
-c = pix[4,4][2]
-if (a+b+c<400):
-    print('R=',a,'G=',b,'B=',c)
-    print (os.path.realpath("red_410.png") )
-else:
-    print("Noooo")
+from PIL import Image, ImageDraw
+text = "Python Imaging Library in Habr :)"
+color = (0, 0, 120)
+img = Image.new('RGB', (100, 50), color)
+imgDrawer = ImageDraw.Draw(img)
+imgDrawer.text((10, 20), text)
+img.save("pil-example.png")
