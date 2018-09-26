6f65c434550d56b2bd0f38ec854ad852dbf56621
Миша <mvbdoc1@gmail.com>
2018-09-26 12:44:47 +0300

    250918

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw
 # https://habr.com/post/163663/
 mode = int(input('mode:')) #Считываем номер преобразования.
-image = Image.open("temp.jpg") #Открываем изображение.
+image = Image.open("face.jpg") #Открываем изображение.
+#Сделац открывание фото в цикле
