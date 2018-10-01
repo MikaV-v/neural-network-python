ae50f68efaf78d85040a8eb0d308fcc25bafd15c
Миша <mvbdoc1@gmail.com>
2018-10-01 22:51:10 +0300

    01.10.2018 neiro testing

--- testig neero.py
+++ testig neero.py
@@ -1,42 +1,43 @@
 import numpy as np
-f = 0
 
-# Сигмоида
+
+# sigmoid function
 def nonlin(x, deriv=False):
     if (deriv == True):
-        return f(x) * (1 - f(x))
+        return x * (1 - x)
     return 1 / (1 + np.exp(-x))
 
 
-# набор входных данных
+# input dataset
 X = np.array([[0, 0, 1],
               [0, 1, 1],
               [1, 0, 1],
               [1, 1, 1]])
 
-# выходные данные
+# output dataset
 y = np.array([[0, 0, 1, 1]]).T
 
-# сделаем случайные числа более определёнными
+# seed random numbers to make calculation
+# deterministic (just a good practice)
 np.random.seed(1)
 
-# инициализируем веса случайным образом со средним 0
+# initialize weights randomly with mean 0
 syn0 = 2 * np.random.random((3, 1)) - 1
 
 for iter in range(10000):
-    # прямое распространение
+    # forward propagation
     l0 = X
     l1 = nonlin(np.dot(l0, syn0))
 
-    # насколько мы ошиблись?
+    # how much did we miss?
     l1_error = y - l1
 
-    # перемножим это с наклоном сигмоиды
-    # на основе значений в l1
-    l1_delta = l1_error * nonlin(l1, True)  # !!!
+    # multiply how much we missed by the
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * nonlin(l1, True)
 
-    # обновим веса
-    syn0 += np.dot(l0.T, l1_delta)  # !!!
+    # update weights
+    syn0 += np.dot(l0.T, l1_delta)
 
-print("Выходные данные после тренировки: ")
+print("Output After Training: ")
 print(l1)
